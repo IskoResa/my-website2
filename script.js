f91390a5423014f4695132370b028e5b8433f4c3
IskoResa <matheresabisquera@gmail.com>
2026-04-27 14:32:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } 
+import { getAuth, signInWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
-// Your config
 const firebaseConfig = {
-  apiKey: "AIzaSyArtxIcDqSn1ZyKr6EVV3PCZen6EeakeSI",
+  apiKey: "AIzaSy...",
   authDomain: "my-website-4375a.firebaseapp.com",
   projectId: "my-website-4375a",
 };
 
-// Initialize
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Login
-window.login = function () {
+function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
@@ -24,13 +20,15 @@ window.login = function () {
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       console.log("LOGIN SUCCESS", userCredential.user);
-
       document.getElementById("status").innerText = "Logged in!";
 
-      window.location.href = "dashboard.html";
+      // TEMPORARILY REMOVE THIS IF NO DASHBOARD
+      // window.location.href = "dashboard.html";
     })
     .catch((err) => {
       console.log("LOGIN ERROR:", err.message);
       document.getElementById("status").innerText = err.message;
     });
-};
+}
+
+document.getElementById("loginBtn").addEventListener("click", login);
